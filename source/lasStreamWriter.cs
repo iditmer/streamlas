03a79467585ad2ce473731000c126717f06799a3
iditmer <90423241+iditmer@users.noreply.github.com>
2025-03-28 10:53:24 -0800

    Implement file signature output

---
+++ source/lasStreamWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace streamlas
+{
+    public class lasStreamWriter : IDisposable
+    {
+        internal BinaryWriter writer;
+
+        public lasStreamWriter(lasStreamReader reader, lasPointRecord point, string path)
+        {
+            writer = new BinaryWriter(File.Create(path));
+            writer.Write(Encoding.ASCII.GetBytes("LASF"));
+        }
+
+        public void Dispose() { writer.Dispose(); }
+    }
+}
