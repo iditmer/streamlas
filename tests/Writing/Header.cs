fe70034a7d6c2122127b7377869a17197cc55753
iditmer <90423241+iditmer@users.noreply.github.com>
2025-03-28 17:22:59 -0800

    Add test of writing GUID fields

--- tests/Writing/Header.cs
+++ tests/Writing/Header.cs
@@ -9,6 +9,22 @@ namespace Writing
     public class Header
     {
         [TestMethod]
+        public void ProjectID()
+        {
+            foreach (var file in TestData.BaseFiles)
+            {
+                string test_path = TestData.WriteTestPath(file);
+                using (lasStreamReader lr = new lasStreamReader(test_path))
+                {
+                    Assert.AreEqual(file.GUID1, lr.GUID1);
+                    Assert.AreEqual(file.GUID2, lr.GUID2);
+                    Assert.AreEqual(file.GUID3, lr.GUID3);
+                    for (int i = 0; i < 8; i++) Assert.AreEqual(file.GUID4[i], lr.GUID4[i]);
+                }
+            }
+        }
+
+        [TestMethod]
         public void VersionMinor()
         {
             foreach (var file in TestData.BaseFiles)
