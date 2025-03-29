3019332d4b9732152c4b6301e72123feacfdaf7f
iditmer <90423241+iditmer@users.noreply.github.com>
2025-03-29 13:52:18 -0800

    Add remaining tests of writing point attributes (see details)
    All of these tests pass presently without any changes to the
    writer object as the current flow of data is to read full point
    records and write them without any modification. More deliberate
    tests will be required to confirm modified point records are
    written correctly, but these serve as a safety net.

--- tests/Writing/Points.cs
+++ tests/Writing/Points.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using streamlas;
+using System;
 using tests;
 
 namespace Writing
@@ -24,6 +25,210 @@ namespace Writing
                         Assert.AreEqual(ref_pts[i].X, pt.X, 1e-6);
                         Assert.AreEqual(ref_pts[i].Y, pt.Y, 1e-6);
                         Assert.AreEqual(ref_pts[i].Z, pt.Z, 1e-6);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Intensity()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        Assert.AreEqual(ref_pts[i].Intensity, pt.Intensity);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Returns()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        Assert.AreEqual(ref_pts[i].Return, pt.ReturnNumber);
+                        Assert.AreEqual(ref_pts[i].NumberReturns, pt.NumberReturns);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Classification()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        Assert.AreEqual(ref_pts[i].Classification, pt.Classification);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void BitFlags()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        Assert.AreEqual(ref_pts[i].SyntheticFlag, pt.SyntheticFlag);
+                        Assert.AreEqual(ref_pts[i].KeypointFlag, pt.KeypointFlag);
+                        Assert.AreEqual(ref_pts[i].WithheldFlag, pt.WithheldFlag);
+                        Assert.AreEqual(ref_pts[i].ScanDirectionFlag, pt.ScanDirectionFlag);
+                        Assert.AreEqual(ref_pts[i].EdgeOfFlightLineFlag, pt.EdgeOfFlightLineFlag);
+
+                        if (lr.PointFormat < 6)
+                        {
+                            var ex = Assert.Throws<InvalidOperationException>(() => pt.OverlapFlag());
+                            Assert.IsTrue(ex.Message.Contains("Overlap flag not defined for point format"));
+                        }
+                        else Assert.AreEqual(ref_pts[i].OverlapFlag, pt.OverlapFlag());
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ScannerChannel()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        if (lr.PointFormat < 6)
+                        {
+                            var ex = Assert.Throws<InvalidOperationException>(() => pt.ScannerChannel());
+                            Assert.IsTrue(ex.Message.Contains("Scanner Channel field not defined for point format"));
+                        }
+                        else Assert.AreEqual(ref_pts[i].ScannerChannel, pt.ScannerChannel());
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void UserData()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        Assert.AreEqual(ref_pts[i].UserData, pt.UserData);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ScanAngle()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        double test_val = ref_pts[i].ScanAngle * 0.006;
+                        if (lr.PointFormat < 6) Assert.AreEqual(Math.Round(test_val), pt.ScanAngle);
+                        else Assert.AreEqual(test_val, pt.ScanAngle);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SourceID()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        Assert.AreEqual(ref_pts[i].SourceID, pt.SourceID);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Timestamps()
+        {
+            foreach (var info in TestData.BaseFiles)
+            {
+                string in_path = TestData.WriteTestPath(info);
+                using (lasStreamReader lr = new lasStreamReader(in_path))
+                {
+                    var ref_pts = TestData.BaseFilePoints[lr.PointFormat];
+                    lasPointRecord pt = new lasPointRecord(lr);
+
+                    for (int i = 0; i < ref_pts.Count; i++)
+                    {
+                        pt.ReadFrom(lr);
+                        if (!lr.HasTimestamps)
+                        {
+                            var ex = Assert.Throws<InvalidOperationException>(() => pt.Timestamp());
+                            Assert.IsTrue(ex.Message.Contains("Timestamp not stored in point format"));
+                        }
+                        else Assert.AreEqual(ref_pts[i].Timestamp, pt.Timestamp());
                     }
                 }
             }
