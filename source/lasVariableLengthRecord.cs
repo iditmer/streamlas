9715caeb481048736c648212385cc07b361c000a
iditmer <90423241+iditmer@users.noreply.github.com>
2025-04-14 10:10:17 -0800

    Implement reading per VLR Description

--- source/lasVariableLengthRecord.cs
+++ source/lasVariableLengthRecord.cs
@@ -8,6 +8,7 @@ namespace streamlas
     {
         public string UserID { get; private set; } = "";
         public UInt16 RecordID { get; private set; }
+        public string Description { get; private set; } = "";
 
         internal lasVariableLengthRecord(BinaryReader r)
         {
@@ -15,7 +16,8 @@ namespace streamlas
             UserID = Encoding.UTF8.GetString(r.ReadBytes(16)).TrimEnd('\0');
             RecordID = r.ReadUInt16();
             UInt16 length_after_header = r.ReadUInt16();
-            r.ReadBytes(32 + length_after_header);
+            Description = Encoding.UTF8.GetString(r.ReadBytes(32)).TrimEnd('\0');
+            r.ReadBytes(length_after_header);
         }
     }
 }
